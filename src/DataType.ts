5774450eb2b5ab2a49548882f52114231cabe6ed
Eduardo Souza <edcampossouza@gmail.com>
2023-06-06 16:23:47 -0300

    feat: create basic types

--- src/DataType.ts
+++ src/DataType.ts
@@ -1,3 +1,18 @@
-export class DataType {
+class DataType {
   constructor(public name: string) {}
 }
+
+const IntType = new DataType("Int");
+const StringType = new DataType("String");
+const BooleanType = new DataType("Boolean");
+const DecimalType = new DataType("Decimal");
+const DateTimeType = new DataType("DateTime");
+
+export {
+  IntType,
+  StringType,
+  BooleanType,
+  DecimalType,
+  DateTimeType,
+  DataType,
+};
